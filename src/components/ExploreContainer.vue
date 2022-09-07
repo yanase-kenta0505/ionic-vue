63ba8352b7be4b991bd8825501978b28ce0fcb77
yanasekenta <ynsknt0505@gmail.com>
2022-09-07 17:24:59 +0900

    レスポンシブ対応

--- src/components/ExploreContainer.vue
+++ src/components/ExploreContainer.vue
@@ -2,17 +2,36 @@
   <div id="container">
     <strong>{{ name }}</strong>
     <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+  <ion-grid> 
+    <ion-row>
+      <ion-col size-xs="12" >
+        <div class="test" >test</div>
+        <ion-button color="primary">test</ion-button>
+      </ion-col>
+      <ion-col size-xs="12">
+        <div>2 of 4</div>
+      </ion-col>
+      <ion-col size-xs="12">
+        <div>3 of 4</div>
+      </ion-col>
+      <ion-col size-xs="12">
+        <div>4 of 4</div>
+      </ion-col>
+    </ion-row>
+</ion-grid>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { IonButton, IonGrid, IonRow, IonCol } from '@ionic/vue';
 
 export default defineComponent({
   name: 'ExploreContainer',
   props: {
     name: String
-  }
+  },
+  components: { IonButton, IonGrid, IonRow, IonCol }
 });
 </script>
 
@@ -24,6 +43,7 @@ export default defineComponent({
   right: 0;
   top: 50%;
   transform: translateY(-50%);
+  /* height: 100% !important; */
 }
 
 #container strong {
@@ -40,5 +60,10 @@ export default defineComponent({
 
 #container a {
   text-decoration: none;
+}
+
+.test {
+  background-color: red;
+  height: 100px;
 }
 </style>
