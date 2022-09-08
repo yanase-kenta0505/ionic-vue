c78090d04af270130fc806fc5b5ae7317171771d
yanasekenta <ynsknt0505@gmail.com>
2022-09-08 13:51:21 +0900

    electron追加

---
+++ electron/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'test.example.app',
+  appName: 'test1',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
